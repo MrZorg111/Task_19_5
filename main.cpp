d4a42109e507b8b865981c6997b4864ce14ae4cb
MrZorg111 <yodji333@mail.ru>
2022-07-23 00:40:51 +0300

    Second solution

--- main.cpp
+++ main.cpp
@@ -2,20 +2,22 @@
 #include <string>
 #include <fstream>
 
+
 int main() {
-    int step = 0, experts = 0, viewers = 0;
+    std::string involved[13];
+    int step = 0, experts = 0, viewers = 0, a = 0;
     std::string text, answer, answer_user, pos, book = " ", t_question, t_answer;
     std::fstream reader_f, reader_s, reader_q, reader_a;
+    std::cout << "Please spin the top: " << std::endl;
+    std::cin >> step;
+
     while(experts != 6 && viewers != 6) {
-        std::cout << "Please spin the top: " << std::endl;
-        std::cin >> step;
         reader_f.open("P:\\WWW\\question.txt");
         reader_s.open("P:\\WWW\\answer.txt");
         for (int i = 1; i <= step; i++) {
             reader_f >> text;
             reader_s >> answer;
         }
-        std::cout << answer << "\n";
         reader_q.open(text);
         reader_a.open(answer);
         while (!reader_q.eof()) {
@@ -34,6 +36,7 @@ int main() {
             viewers++;
             std::cout << "Viewers get a score!\n";
         }
+        a++;
         reader_q.close(), reader_a.close(), reader_f.close(), reader_s.close();
     }
     if (experts == 6) {
