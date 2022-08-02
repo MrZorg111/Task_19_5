e3bd99cfcf9376d206cbaf7adde26a3a61e54a71
MrZorg111 <yodji333@mail.ru>
2022-08-02 07:47:58 +0300

    Вывод вопроса в консоль и написание проверки повторения вопроса.

--- main.cpp
+++ main.cpp
@@ -3,47 +3,42 @@
 #include <fstream>
 
 
+
+void rec (std::string r[13][2]) {
+    std::fstream reader_ad_q, reader_ad_ans;
+    std::string address_question, address_answer;
+    reader_ad_q.open("P:\\WWW\\question.txt"), reader_ad_ans.open("P:\\WWW\\answer.txt");
+    for (int i = 0; i < 13; i++) {
+        reader_ad_q >> address_question, reader_ad_ans >> address_answer;
+
+        r[i][0] = address_question, r[i][1] = address_answer;
+    }
+    reader_ad_ans.close(), reader_ad_q.close();
+}
+void replacement (std::string arr[13][2], int pos) {
+    arr[pos][0] = "0";
+}
+
 int main() {
-    std::string involved[13];
-    int step = 0, experts = 0, viewers = 0, a = 0;
-    std::string text, answer, answer_user, pos, book = " ", t_question, t_answer;
-    std::fstream reader_f, reader_s, reader_q, reader_a;
-    std::cout << "Please spin the top: " << std::endl;
+    std::string involved[13][2];
+    rec(involved);
+    int step, experts = 0, viewers = 0, a = 0;
+    std::string  answer_user, pos, question, t_question, t_answer;
+    std::fstream reader_q, reader_a;
+    std::cout << "Enter the rotation step of the drum: ";
     std::cin >> step;
 
-    while(experts != 6 && viewers != 6) {
-        reader_f.open("P:\\WWW\\question.txt");
-        reader_s.open("P:\\WWW\\answer.txt");
-        for (int i = 1; i <= step; i++) {
-            reader_f >> text;
-            reader_s >> answer;
+    while(involved[step - 1][0] == "0") {
+        step++;
+        if (step == 12) {
+            step = 1;
         }
-        reader_q.open(text);
-        reader_a.open(answer);
-        while (!reader_q.eof()) {
-            reader_q >> t_question;
-            book += t_question + " ";
-        }
-        std::cout << book << "\n";
-        book = " ";
-        std::cout << "Enter you answer: ";
-        std::cin >> answer_user;
-        reader_a >> t_answer;
-        if (answer_user == t_answer) {
-            experts++;
-            std::cout << "Experts get a score!\n";
-        } else {
-            viewers++;
-            std::cout << "Viewers get a score!\n";
-        }
-        a++;
-        reader_q.close(), reader_a.close(), reader_f.close(), reader_s.close();
     }
-    if (experts == 6) {
-        std::cout << "Experts win!!!";
-    }else {
-        std::cout << "Viewers win!!!";
+    reader_q.open(involved[step - 1][0]);
+    replacement(involved, step);
+    while(!reader_q.eof()) {
+        reader_q >> t_question;
+        question += t_question + " ";
     }
-
     return 0;
 }
