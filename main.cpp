f5ad1cfc0437414eee11a1acd3a1b7b8794ae3e2
MrZorg111 <yodji333@mail.ru>
2022-08-09 07:23:17 +0300

    Финальная версия

--- main.cpp
+++ main.cpp
@@ -18,27 +18,61 @@ void rec (std::string r[13][2]) {
 void replacement (std::string arr[13][2], int pos) {
     arr[pos][0] = "0";
 }
+int check_step (std::string a[13][2], int b) {
+    if (a[b][0] == "0") {
+        do {
+            if (b == 12) {
+                b = -1;
+            }
+            b++;
+
+        }while(a[b][0] == "0");
+        return b;
+    }
+    else {
+        return b;
+    }
+}
 
 int main() {
     std::string involved[13][2];
     rec(involved);
-    int step, experts = 0, viewers = 0, a = 0;
+    int step, experts = 0, viewers = 0;
     std::string  answer_user, pos, question, t_question, t_answer;
     std::fstream reader_q, reader_a;
-    std::cout << "Enter the rotation step of the drum: ";
-    std::cin >> step;
+    while(experts < 6 && viewers < 6) {
+        std::cout << "Enter the rotation step of the drum: ";
+        std::cin >> step;
+        step = check_step(involved, step - 1);
+        reader_q.open(involved[step][0]);
+        replacement(involved, step);
+        while (!reader_q.eof()) {
+            reader_q >> t_question;
+            question += t_question + " ";
+        }
+        reader_q.close();
+        std::cout << question << "\n";
+        question = "";
 
-    while(involved[step - 1][0] == "0") {
-        step++;
-        if (step == 12) {
-            step = 1;
+        std::cout << "Enter your answer: ";
+        std::cin >> answer_user;
+
+        reader_a.open(involved[step][1]);
+        reader_a >> t_answer;
+        reader_a.close();
+        if (answer_user == t_answer) {
+            std::cout << "Viewers get a point! \n";
+            viewers++;
+        } else {
+            std::cout << "Experts get a point! \n";
+            experts++;
         }
     }
-    reader_q.open(involved[step - 1][0]);
-    replacement(involved, step);
-    while(!reader_q.eof()) {
-        reader_q >> t_question;
-        question += t_question + " ";
+    if (viewers == 6) {
+        std::cout << "Viewers win!!!";
+    }else {
+        std::cout << "Experts win!!!";
     }
+
     return 0;
 }
